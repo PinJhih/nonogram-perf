66058f951f14a3d54df68446963f226b1e1718c5
PinJhih <isaacwang29@gmail.com>
2023-10-31 13:37:54 +0800

    perf: Apply heuristic algorithm on getting unpainted pixels

--- src/puzzle/board.cc
+++ src/puzzle/board.cc
@@ -1,5 +1,7 @@
 #include "puzzle/board.h"
 
+#include <algorithm>
+#include <array>
 #include <cstdio>
 #include <cstring>
 
@@ -24,16 +26,15 @@ Board::Board() {
 			unpainted.insert(hashIndex(i, j));
 		}
 	}
+
+	memset(paintedInRow, 0, sizeof(Byte) * BOARD_SIZE);
+	memset(paintedInCol, 0, sizeof(Byte) * BOARD_SIZE);
 }
 
 Board::Board(const Board& h) {
 	*this = h;
-}
-
-Byte Board::get(int i, int j) {
-	if (i == 0 || j == 0)
-		return 0;
-	return g[i - 1].get(j);
+	memcpy(paintedInRow, h.paintedInRow, sizeof(Byte) * BOARD_SIZE);
+	memcpy(paintedInCol, h.paintedInCol, sizeof(Byte) * BOARD_SIZE);
 }
 
 Board& Board::set(int i, int j, Byte c) {
@@ -42,6 +43,7 @@ Board& Board::set(int i, int j, Byte c) {
 
 	short p = hashIndex(i, j);
 	if (unpainted.count(p) != 0) {
+		paintedInRow[i]++, paintedInCol[j]++;
 		g[i - 1].set(j, c);
 		g[j + 24].set(i, c);
 
@@ -50,34 +52,6 @@ Board& Board::set(int i, int j, Byte c) {
 			state = SOLVED;
 	}
 	return *this;
-}
-
-Line Board::getLine(int i) {
-	return Line(g[i - 1]);
-}
-
-State Board::getState() {
-	return state;
-}
-
-void Board::setState(State s) {
-	state = s;
-}
-
-bool Board::finished() {
-	return state == CONFLICT || state == SOLVED;
-}
-
-bool Board::conflict() {
-	return state == CONFLICT;
-}
-
-bool Board::solved() {
-	return state == SOLVED;
-}
-
-bool Board::painted() {
-	return state == PAINTED;
 }
 
 bool Board::isPainted(int i, int j) {
@@ -92,8 +66,19 @@ void Board::pickUnpainted(int& i, int& j) {
 	toIndex(p, i, j);
 }
 
-const PixelSet& Board::unpaintedPixels() {
-	return unpainted;
+const vector<pair<short, Byte>> Board::unpaintedPixels() {
+	vector<pair<short, Byte>> pixels;
+	for (short p : unpainted) {
+		int i, j;
+		toIndex(p, i, j);
+		pixels.push_back({p, paintedInRow[i] + paintedInCol[j]});
+	}
+
+	auto cmp = [](const pair<short, Byte>& a, const pair<short, Byte>& b) {
+		return a.second > b.second;
+	};
+	sort(pixels.begin(), pixels.end(), cmp);
+	return pixels;
 }
 
 void Board::print() {
