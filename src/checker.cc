d5add51c7856447785e8ca5d96e4539d8dddcc4f
PinJhih <isaacwang29@gmail.com>
2023-10-31 22:00:05 +0800

    fix: Use fgets() instead of gets() for compatibility in Linux

--- src/checker.cc
+++ src/checker.cc
@@ -26,7 +26,7 @@ int readPuzzle(FILE* input, Description d[BOARD_SIZE * 2]) {
 	return t;
 }
 
-bool check(char b[25][50], Description* d) {
+bool check(char b[25][64], Description* d) {
 	for (int i = 0; i < 25; i++) {
 		int m = 0, n = 0;
 		Byte row[15], col[15];
@@ -67,21 +67,21 @@ bool check(char b[25][50], Description* d) {
 int main() {
 	int t;
 	double time, total = 0;
-	char buffer[16];
-	char board[25][50];
+	char state[16];
+	char board[25][64];
 	FILE* file = fopen("input.txt", "r");
-	while (scanf("\nCase %d: %s.\n", &t, buffer) != EOF) {
-		gets(board[0]);
+	while (scanf("\nCase %d: %s.\n", &t, state) != EOF) {
+		fgets(board[0], sizeof(char) * 50, stdin);
 		for (int i = 0; i < 25; i++) {
-			gets(board[i]);
+			fgets(board[i], sizeof(char) * 64, stdin);
 		}
 		scanf("\nRunning Time: %lf sec.", &time);
 		total += time;
 
 		printf(YEL_BOLD "\nCase %d", t);
-		printf(RESET ": %s\n", buffer);
+		printf(RESET ": %s\n", state);
 		for (int i = 0; i < 25; i++) {
-			printf("%s\n", board[i]);
+			printf("%s", board[i]);
 		}
 
 		Description d[50];
